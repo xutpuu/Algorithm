521fa4e6c574a2d8ddd026a06b89d7fbaeb9ced7
Egor Shatrov <xutpuu@gmail.com>
2019-04-08 23:55:34 +0300

    commit some solutions

--- linkedLists.py
+++ linkedLists.py
@@ -1,3 +1,13 @@
+"""The LinkedList code from before is provided below.
+Add three functions to the LinkedList.
+"get_position" returns the element at a certain position.
+The "insert" function will add an element to a particular
+spot in the list.
+"delete" will delete the first element with that
+particular value.
+Then, use "Test Run" and "Submit" to run the test cases
+at the bottom."""
+
 class Element(object):
     def __init__(self, value):
         self.value = value
@@ -15,8 +25,7 @@ class LinkedList(object):
             current.next = new_element
         else:
             self.head = new_element
-
-
+            
     def get_position(self, position):
         """Get an element from a particular position.
         Assume the first position is "1".
@@ -25,30 +34,25 @@ class LinkedList(object):
         current = self.head
         if position < 1:
             return None
-        while current and counter <= position:
-            if counter == position:
-                return current
-            current = current.next
-            counter += 1
-        return None
+        
     
     def insert(self, new_element, position):
         """Insert a new node at the given position.
         Assume the first position is "1".
         Inserting at position 3 means between
         the 2nd and 3rd elements."""
-        counter = 1
+        pass
+    
+    
+    def delete(self, value):
+        """Delete the first node with a given value."""
         current = self.head
-        if position > 1:
-            while current and counter < position:
-                if counter == position - 1:
-                    new_element.next = current.next
-                    current.next = new_element
+        if self.head:
+            while current.value == value:
                 current = current.next
-                counter += 1
-        elif position == 1:
-            new_element.next = self.head
-            self.head = new_element
+        
+
+        pass
 
 # Test cases
 # Set up some Elements
@@ -64,11 +68,20 @@ ll.append(e3)
 
 # Test get_position
 # Should print 3
-#print(ll.head.next.next.value)
+print(ll.head.next.next.value)
 # Should also print 3
-#print(ll.get_position(3).value)
+print(ll.get_position(3).value)
 
 # Test insert
 ll.insert(e4,3)
 # Should print 4 now
-#print(ll.get_position(3).value)
+print(ll.get_position(3).value)
+
+# Test delete
+ll.delete(1)
+# Should print 2 now
+print(ll.get_position(1).value)
+# Should print 4 now
+print(ll.get_position(2).value)
+# Should print 3 now
+print(ll.get_position(3).value)
--- list.py
+++ list.py
@@ -1,3 +1,13 @@
+"""The LinkedList code from before is provided below.
+Add three functions to the LinkedList.
+"get_position" returns the element at a certain position.
+The "insert" function will add an element to a particular
+spot in the list.
+"delete" will delete the first element with that
+particular value.
+Then, use "Test Run" and "Submit" to run the test cases
+at the bottom."""
+
 class Element(object):
     def __init__(self, value):
         self.value = value
@@ -15,8 +25,7 @@ class LinkedList(object):
             current.next = new_element
         else:
             self.head = new_element
-
-
+            
     def get_position(self, position):
         """Get an element from a particular position.
         Assume the first position is "1".
@@ -49,6 +58,21 @@ class LinkedList(object):
         elif position == 1:
             new_element.next = self.head
             self.head = new_element
+    
+    
+    def delete(self, value):
+        """Delete the first node with a given value."""
+        current = self.head
+        previous = None
+        while current.value != value and current.next:
+            previous = current
+            current = current.next
+        if current.value == value:
+            if previous:
+                previous.next = previous
+            else:
+                self.head = current.next
+
 
 # Test cases
 # Set up some Elements
@@ -64,11 +88,20 @@ ll.append(e3)
 
 # Test get_position
 # Should print 3
-#print(ll.head.next.next.value)
+print(ll.head.next.next.value)
 # Should also print 3
-#print(ll.get_position(3).value)
+print(ll.get_position(3).value)
 
 # Test insert
 ll.insert(e4,3)
 # Should print 4 now
-#print(ll.get_position(3).value)
+print(ll.get_position(3).value)
+
+# Test delete
+ll.delete(1)
+# Should print 2 now
+print(ll.get_position(1).value)
+# Should print 4 now
+print(ll.get_position(2).value)
+# Should print 3 now
+print(ll.get_position(3).value)
